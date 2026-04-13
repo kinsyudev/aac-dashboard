da69472d0eb1be0a2170aea5d8ff9bb967f13e01
kinsyu <miguel@kinsyu.dev>
2026-04-13 09:26:38 +0200

    added railway to server

--- apps/tanstack-start/src/auth/server.ts
+++ apps/tanstack-start/src/auth/server.ts
@@ -7,7 +7,7 @@ import { getBaseUrl } from "~/lib/url";
 
 export const auth = initAuth({
   baseUrl: getBaseUrl(),
-  productionUrl: `https://${env.VERCEL_PROJECT_PRODUCTION_URL ?? "turbo.t3.gg"}`,
+  productionUrl: `https://${env.RAILWAY_PUBLIC_DOMAIN ?? "aac.kinsyu.dev"}`,
   secret: env.AUTH_SECRET,
   discordClientId: env.AUTH_DISCORD_ID,
   discordClientSecret: env.AUTH_DISCORD_SECRET,
